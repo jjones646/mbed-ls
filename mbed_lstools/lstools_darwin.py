1e59b378400e10b768de86a3fcc4c3127e47bf2d
Jon <jjones646@gmail.com>
2016-02-20 05:46:05 -0500

    adding os x el capitan support with verbose debug flag outputs

--- mbed_lstools/lstools_darwin.py
+++ mbed_lstools/lstools_darwin.py
@@ -15,22 +15,41 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import sys
 import re
 import subprocess
 import plistlib
+import logging
 
 from lstools_base import MbedLsToolsBase
+from term_formats import fixedWidthFormat, treeLogger
+
 
 class MbedLsToolsDarwin(MbedLsToolsBase):
-    """ MbedLsToolsDarwin supports mbed-enabled platforms detection on Mac OS X
+    """ MbedLsToolsDarwin supports mbed-enabled platforms detection on
+    Mac OS X.
     """
 
     mbed_volume_name_match = re.compile(r'(\bmbed\b|\bSEGGER MSD\b)', re.I)
 
+    def __init__(self, debug=False):
+        MbedLsToolsBase.__init__(self)
+        logger = logging.getLogger(__name__)
+        if debug:
+            logger.setLevel(logging.DEBUG)
+
+        # set the logging format
+        ww = 18
+        ch = logging.StreamHandler(sys.stdout)
+        ch.setFormatter(fixedWidthFormat(width=ww))
+        logger.addHandler(ch)
+        # pass the logging module into the treeLogger class
+        # that it will use
+        self.treeShow = treeLogger(log_module=logger, log_prefix_width=ww)
+
     def list_mbeds(self):
         """ returns mbed list with platform names if possible
         """
-
         result = []
 
         # {volume_id: {serial:, vendor_id:, product_id:, tty:}}
@@ -49,14 +68,8 @@ class MbedLsToolsDarwin(MbedLsToolsBase):
             valid_volumes[key] = volumes[key]
 
         # put together all of that info into the expected format:
-        result =  [
-            {
-                'mount_point': mounts[v],
-                'serial_port': volumes[v]['tty'],
-                  'target_id': self.target_id(volumes[v]),
-              'platform_name': self.platform_name(self.target_id(volumes[v]))
-            } for v in valid_volumes
-        ]
+        result = [{'mount_point': mounts[v], 'serial_port': volumes[v]['tty'], 'target_id': self.target_id(
+            volumes[v]), 'platform_name': self.platform_name(self.target_id(volumes[v]))} for v in valid_volumes]
 
         self.ERRORLEVEL_FLAG = 0
 
@@ -69,22 +82,25 @@ class MbedLsToolsDarwin(MbedLsToolsBase):
 
             if result[i]['mount_point']:
                 # Deducing mbed-enabled TargetID based on available targetID definition DB.
-                # If TargetID from USBID is not recognized we will try to check URL in mbed.htm
-                htm_target_id = self.get_mbed_htm_target_id(result[i]['mount_point'])
+                # If TargetID from USBID is not recognized we will try to check
+                # URL in mbed.htm
+                htm_target_id = self.get_mbed_htm_target_id(
+                    result[i]['mount_point'])
                 if htm_target_id:
                     result[i]['target_id_usb_id'] = result[i]['target_id']
                     result[i]['target_id'] = htm_target_id
-                    result[i]['platform_name'] = self.platform_name(htm_target_id[:4])
+                    result[i]['platform_name'] = self.platform_name(
+                        htm_target_id[:4])
                 result[i]['target_id_mbed_htm'] = htm_target_id
 
         return result
-
 
     def get_mount_points(self):
         ''' Returns map {volume_id: mount_point} '''
 
         # list disks, this gives us disk name, and volume name + mount point:
-        diskutil_ls = subprocess.Popen(['diskutil', 'list', '-plist'], stdout=subprocess.PIPE)
+        diskutil_ls = subprocess.Popen(
+            ['diskutil', 'list', '-plist'], stdout=subprocess.PIPE)
         disks = plistlib.readPlist(diskutil_ls.stdout)
         diskutil_ls.wait()
 
@@ -98,80 +114,156 @@ class MbedLsToolsDarwin(MbedLsToolsBase):
 
         return r
 
-
     def get_mbed_volumes(self):
-        ''' returns a map {volume_id: {serial:, vendor_id:, product_id:, tty:}'''
+        ''' returns a map {volume_id: {serial:, vendor_id:, product_id:, tty:}
+        '''
 
-        # to find all the possible mbed volumes, we look for registry entries
-        # under all possible USB bus which have a "BSD Name" that starts with "disk"
-        # (i.e. this is a USB disk), and have a IORegistryEntryName that
-        # matches /\cmbed/
-        # Once we've found a disk, we can search up for a parent with a valid
-        # serial number, and then search down again to find a tty that's part
-        # of the same composite device
-        # ioreg -a -r -n <usb_controller_name> -l
-        usb_controllers = ['AppleUSBXHCI', 'AppleUSBUHCI', 'AppleUSBEHCI', 'AppleUSBOHCI']
-        usb_bus = []
+        def has_children(obj, child_key='IORegistryEntryChildren'):
+            if obj is not None and 'IORegistryEntryChildren' in obj:
+                return True
+            return False
 
-        for usb_controller in usb_controllers:	
-            ioreg_usb = subprocess.Popen(['ioreg', '-a', '-r', '-n', usb_controller, '-l'], stdout=subprocess.PIPE)
-            
-	    try:
-                usb_bus = usb_bus + plistlib.readPlist(ioreg_usb.stdout)
-	    except:
-		# Catch when no output is returned from ioreg command
+        def get_children(obj, child_key='IORegistryEntryChildren'):
+            if has_children(obj, child_key=child_key):
+                return obj[child_key]
+            return None
+
+        def is_mbed_root(obj):
+            if 'sessionID' in obj and 'bcdDevice' in obj:
+                return True
+            return False
+
+        def get_drive_info():
+            ''' finds and returns a list of connected usb drives. '''
+            res = None
+            q = subprocess.Popen(
+                ['system_profiler', 'SPUSBDataType', '-detailLevel', 'full', '-xml'], stdout=subprocess.PIPE)
+            try:
+                res = plistlib.readPlist(q.stdout)
+            except:
                 pass
+            finally:
+                q.wait()
 
-            ioreg_usb.wait()
+            return res[0]['_items']
+
+        def filter_io_reg(obj):
+            ''' filters the queried data and returns an array containing
+                all of the mbed entry trees.
+            '''
+            r = []
+            for i in obj:
+                for j in i:
+                    if j == 'IORegistryEntryName' and i[j] == 'Root':
+                        c = i
+                        while has_children(c):
+                            c = get_children(c)
+                            if is_mbed_root(c):
+                                r.append(c)
+                                break
+            return r
+
+        def filter_system_profiler(obj):
+            ''' filters the data from the system_profiler call. '''
+            r = []
+            if not isinstance(obj, list):
+                obj = [obj]
+
+            for o in obj:
+                for x in filter(lambda k: k == '_items', o.keys()):
+                    try:
+                        for i in o['_items']:
+                            # recurse through all of the child nodes
+                            if '_items' in i:
+                                r.extend(filter_system_profiler(i))
+
+                            search_term = i['_name']
+                            if 'manufacturer' in i:
+                                search_term += ' ' + i['manufacturer']
+
+                            if self.mbed_volume_name_match.search(search_term) and 'bsd_name' in i:
+                                r.append(i)
+                    except:
+                        continue
+            return r
+
+        def get_disk_id(obj, sn):
+            ''' returns the bsd name when given a mbed serial number. '''
+            try:
+                for dev in obj:
+                    if 'serial_num' in dev and 'bsd_name' in dev and dev['serial_num'] == sn:
+                        return dev['bsd_name']
+            except:
+                return None
+
+        # Now we query for a tree leading to all BSD client entries of the registry - we
+        # can parse the rest of things from this
+        res = None
+        ioreg_query = subprocess.Popen(
+            ['ioreg', '-a', '-t', '-l', '-r', '-c', 'IOSerialBSDClient'], stdout=subprocess.PIPE)
+        try:
+            res = plistlib.readPlist(ioreg_query.stdout)
+        except:
+            # Catch when no output is returned from ioreg command
+            pass
+        finally:
+            # wait for the results
+            ioreg_query.wait()
+
+        # select each found mbed root tree
+        serial_devs = None
+        mbed_drives = None
+        try:
+            serial_devs = filter_io_reg(res)
+            mbed_drives = filter_system_profiler(get_drive_info())
+        except:
+            pass
+        finally:
+            self.treeShow.show(serial_devs)
+            self.treeShow.show(mbed_drives)
 
         r = {}
 
-        def findTTYRecursive(obj):
-            ''' return the first tty (AKA IODialinDevice) that we can find in the
-                children of the specified object, or None if no tty is present.
-            '''
-            if 'IODialinDevice' in obj:
-                return obj['IODialinDevice']
-            if 'IORegistryEntryChildren' in obj:
-                for child in obj['IORegistryEntryChildren']:
-                    found = findTTYRecursive(child)
-                    if found:
-                        return found
-            return None
+        def set_mbed_devs(obj):
+            """ sets the return value for the parent function, get_mbed_volumes()
+            """
 
-        def findVolumesRecursive(obj, parents):
-            if 'BSD Name' in obj and obj['BSD Name'].startswith('disk') and \
-                    self.mbed_volume_name_match.search(obj['IORegistryEntryName']):
-                disk_id = obj['BSD Name']
-                # now search up through our parents until we find a serial number:
-                usb_info = {
-                        'serial':None,
-                     'vendor_id':None,
-                    'product_id':None,
-                           'tty':None,
-                }
-                for parent in [obj] + parents:
-                    if 'USB Serial Number' in parent:
-                        usb_info['serial'] = parent['USB Serial Number']
-                    if 'idVendor' in parent and 'idProduct' in parent:
-                        usb_info['vendor_id'] = parent['idVendor']
-                        usb_info['product_id'] = parent['idProduct']
-                    if usb_info['serial']:
+            def find_tty(obj):
+                """ return the first tty (AKA IODialinDevice) that we can find in the
+                    children of the specified object, or None if no tty is present.
+                """
+                if isinstance(obj, dict) and 'IODialinDevice' in obj:
+                    return obj['IODialinDevice']
+                if has_children(obj):
+                    return find_tty(get_children(obj))
+                return None
+
+            # select the entries that have mbed in their name
+            for k, v in obj.items():
+                if isinstance(v, dict):
+                    set_mbed_devs(v)
+                elif (k == 'IORegistryEntryName' or k == 'kUSBVendorString') and self.mbed_volume_name_match.search(v):
+                    usb_info = {
+                        'serial': None, 'vendor_id': None, 'product_id': None, 'tty': None, }
+                    if 'idVendor' in obj:
+                        usb_info['vendor_id'] = obj['idVendor']
+                    if 'idProduct' in obj:
+                        usb_info['product_id'] = obj['idProduct']
+                    if 'USB Serial Number' in obj:
+                        usb_info['serial'] = obj['USB Serial Number']
                         # stop at the first one we find (or we'll pick up hubs,
                         # etc.), but first check for a tty that's also a child of
                         # this device:
-                        usb_info['tty'] = findTTYRecursive(parent)
-                        break
-                r[disk_id] = usb_info
-            if 'IORegistryEntryChildren' in obj:
-                for child in obj['IORegistryEntryChildren']:
-                    findVolumesRecursive(child, [obj] + parents)
+                        usb_info['tty'] = find_tty(obj)
+                        # set the bsd name that lines up with the serial
+                        # number
+                        disk_id = get_disk_id(mbed_drives, usb_info['serial'])
+                        r[disk_id] = usb_info
 
-        for obj in usb_bus:
-            findVolumesRecursive(obj, [])
+        for obj in serial_devs:
+            set_mbed_devs(obj)
 
         return r
-
 
     def target_id(self, usb_info):
         if usb_info['serial'] is not None:
---
+++ mbed_lstools/term_formats.py
@@ -0,0 +1,93 @@
+"""
+mbed SDK
+Copyright (c) 2011-2015 ARM Limited
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import logging
+
+
+def getTermDims():
+    """! Returns the terminal dimensions when using the CLI
+        @return [<rows>, <columns>]
+        @details Function that returns the terminal window dimensions of 
+        the currently open terminal at the moment in time when the function
+        is called.
+    """
+    return [int(i) for i in os.popen('stty size', 'r').read().split()]
+
+
+class fixedWidthFormat(logging.Formatter):
+    """ Class that defines a fixed-width format for log message prefixes that
+        are shown before the log message itself.
+    """
+    def __init__(self, width=15):
+        self.width = width
+
+    def format(self, record):
+        max_chars = self.width - len(str(record.lineno))
+        fn = record.filename[:max_chars]
+        if len(record.filename) > self.width:
+            fn = fn[:-3] + '..'
+        a = '{}:{}'.format(fn, record.lineno)
+        return '[{}] {}'.format(a.ljust(self.width), record.msg)
+
+
+class treeLogger:
+    ''' Use the treeLogger class to recursively show large
+        data trees that are an assortment of dicts/lists.
+    '''
+    TAB_PREFIX = 4 * ' '
+    DEPTH = 0
+
+    def __init__(self, log_module, prefix=None, log_prefix_width=0):
+        if prefix is not None:
+            self.tabPrefix = prefix
+        else:
+            self.tabPrefix = self.TAB_PREFIX
+
+        if log_module is None:
+            self.logger = logging.basicConfig(level=logging.INFO)
+            self.logger.setLevel(logging.CRITICAL)
+        else:
+            self.logger = log_module
+
+        self.log_prefix_width = log_prefix_width + 3
+
+    def show_dict(self, d, depth=0):
+        ''' recursively print a dictionary
+        '''
+        # find the max number of characters in all of the keys
+        max_key_width = reduce(lambda a, b: a if (a > b) else b, [len(k) for k in d.keys()])
+
+        for k in filter(lambda k: isinstance(d[k], basestring) or isinstance(d[k], int), d.keys()):
+            self.logger.info('{}{}{}  =>  {}'.format((self.tabPrefix * depth), k, (max_key_width - len(k)) * ' ', d[k]))
+
+        for k in sorted(filter(lambda k: isinstance(d[k], dict), d.keys()), key=lambda fk: len(d[fk])):
+            self.logger.info('{}{}{}'.format(((len(self.tabPrefix) * depth) - 2) * '=', '> ', k))
+            self.show_dict(d[k], depth + 1)
+
+    def show(self, t, depth=0):
+        ''' recursively print an intertangled list/dictionary structure
+            to a given depth within each dictionary
+        '''
+        (h, w) = getTermDims()
+        w -= self.log_prefix_width
+        if isinstance(t, list):
+            for tree in t:
+                self.show(tree, depth=depth)
+                self.logger.info(w * '~')
+        if isinstance(t, dict):
+            self.show_dict(t, depth=depth)
